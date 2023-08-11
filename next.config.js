9a82afaa1109fd9f2048add488431bb44ecbe034
PKLogger <senseicuong@gmail.com>
2023-08-11 22:38:28 +0700

    update package.json

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-// const fs = require('fs')
 const path = require('path')
 const dotenv = require('dotenv')
 const withLess = require('./src/lib/next-with-less')
 withLess.patchNext(require('next/dist/build/webpack/config/blocks/css'))
 
-// const isProd = process.env.NODE_ENV === 'production'
 if (process.env.NODE_ENV === 'production') {
   dotenv.config({ path: path.resolve(process.cwd(), '.env.production') })
 } else {
@@ -13,6 +11,8 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = withLess({
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/gh-pages-test',
   lessLoaderOptions: {
     // it's possible to use additionalData or modifyVars for antd theming
     // read more @ https://ant.design/docs/react/customize-theme
@@ -24,17 +24,6 @@ module.exports = withLess({
       },
     },
   },
-  // Use the CDN in production and localhost for development.
-  // assetPrefix: isProd ? `${process.env.BASE_URL}` : '',
-  // rewrites: async () => nextI18NextRewrites(localeSubpaths),
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/:slug*',
-  //       destination: '/:slug', // Matched parameters can be used in the destination
-  //     },
-  //   ];
-  // },
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
@@ -46,7 +35,6 @@ module.exports = withLess({
       '632466.sgp1.digitaloceanspaces.com',
       'media.meete.co',
     ],
-    // path: '/_next/image',
     loader: 'default',
   },
   i18n: {
@@ -56,22 +44,6 @@ module.exports = withLess({
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
     defaultLocale: 'vi',
-    // This is a list of locale domains and the default locale they
-    // should handle (these are only required when setting up domain routing)
-    /* domains: [
-	    {
-	      domain: 'example.com',
-	      defaultLocale: 'en-US',
-	    },
-	    {
-	      domain: 'example.nl',
-	      defaultLocale: 'nl-NL',
-	    },
-	    {
-	      domain: 'example.fr',
-	      defaultLocale: 'fr',
-	    },
-	  ], */
   },
   serverRuntimeConfig: {
     TOKEN_SECRET: 'c540612b-2391-4b3a-83ae-a4ad5a90aa76',
@@ -84,17 +56,6 @@ module.exports = withLess({
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.externals.push('jquery')
-      /* config.module.rules.push({
-        test: require.resolve('jquery'),
-        use: [{
-          loader: 'expose-loader',
-          options: 'jQuery'
-        },
-        {
-          loader: 'expose-loader',
-          options: '$'
-        }]
-      }) */
     }
     config.resolve.fallback = { fs: false }
 
